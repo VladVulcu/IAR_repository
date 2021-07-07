d68bc215bc179d2c6e1ba18a3d676397c3c86090
VladVulcu <vulcu_vlad2000@yahoo.com>
2021-07-07 13:03:26 +0300

    added a subtraction function

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 int a;
 int b;
 int c;
+int d;
 
 int function_sum(int x, int y) {
   int sum;
@@ -9,10 +10,16 @@ int function_sum(int x, int y) {
   return sum;
 }
 
+int function_sub(int x, int y) {
+	int sub;
+	sub = x-y;
+}
+
 int main(void){
   a=2;
   b=3;
   c = function_sum(a, b);
+  d = function_sub(b, a)
   
   return 0;
   
