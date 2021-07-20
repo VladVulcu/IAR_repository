738304522f516a2ee8c61bd362aac5d927a782b0
VladVulcu <62437233+VladVulcu@users.noreply.github.com>
2021-07-20 15:01:15 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,30 +1,17 @@
-int a;
-int b;
-int c;
-int d;
-int e;
-int f;
+#include "iom324pb.h"
+#include "gpio.h"
+#include "led.h"
+#include "button.h"
+#include "sos.h"
 
-int function_sum(int x, int y) {
-  int sum;
-  sum=x+y;
+
+void main(void)  {
+
+  while(1){
   
-  return sum;
+  led_on_button_pressed(0);
+  led_on_button_pressed(1);
+  led_on_button_pressed(2);
+  led_on_button_pressed(3);
+  }
 }
-
-int function_sub(int x, int y) {
-	int sub;
-	sub = x-y;
-}
-
-int main(void){
-  a=2;
-  b=3;
-  c = function_sum(a, b);
-  d = function_sub(b, a)
-  
-  return 0;
-  
-}
-
-
